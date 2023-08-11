d7f72bed509a455286576c9a2d9592119a96adc5
Krishna8957 <krishnapal7576@gmail.com>
2023-08-11 02:51:23 -0700

    push branch second

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
-console.log("this is the new branch feature1");
+ const express = require("express");
+ app = express();
+ const mongoose = require("mongoose");
+ mongoose.connect("mongodb+srv://krishnapal789:kk789@cluster0.jh8ps5r.mongodb.net/viewdb").then()
+ {
+    app.get("/",function(req,res){
+        res.send("this is the home page");
+     });
+     app.get("/notes",function(req,res){
+        res.send("this is the notes page");
+     });
+ };
+ 
+
+
+
+ app.listen(5000,function(){
+    console.log("Server Started at Port :5000")
+ })
 
